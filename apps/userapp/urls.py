1d39a7989503af398ba603d6cc56b71e0f50d524
Yevgeniy Zhel <21162@wisk.kz>
2020-11-20 11:52:06 +0600

    Добавлена смена пароля

--- apps/userapp/urls.py
+++ apps/userapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+from django.contrib.auth.decorators import login_required
 
 app_name = 'userapp'
 urlpatterns = [
@@ -8,6 +8,23 @@ urlpatterns = [
         'login/',
         auth_views.LoginView.as_view(template_name='userapp/auth/login.html'),
         name='login'
+    ),
+    path(
+        'change_password/',
+        login_required(
+            auth_views.PasswordChangeView.as_view(
+                template_name='userapp/auth/change_password.html',
+                success_url='/password_change_done/',
+            )
+        ),
+        name='change_password',
+    ),
+    path(
+      'password_change_done/',
+      login_required(auth_views.PasswordChangeDoneView.as_view(
+          template_name='userapp/auth/password_change_done.html',
+      )),
+      name='password_change_done'
     ),
     path(
         'logout/',
