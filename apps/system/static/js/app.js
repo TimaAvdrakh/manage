447fa05ecee34629d08d07669facab1f3fc5ed6c
Yevgeniy Zhel <21162@wisk.kz>
2020-11-25 10:57:47 +0600

    Добавлен js getFolderList для получения списка папок заданий

--- apps/system/static/js/app.js
+++ apps/system/static/js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation()
+
+
+var HOST = "http://127.0.0.1:8011/"
+
+function getFolderList(selector) {
+    $.ajax({
+        type: "GET",
+        url: HOST + "api/folder_task/?format=json",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            selector.jstree(data);
+        },
+        error: function (errMsg) {
+            alert(errMsg);
+        }
+    });
+}
